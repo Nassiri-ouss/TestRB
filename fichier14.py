514fa663451514bdc6f6c833ed5f057230079af7
Nassiri-ouss <oussaman@gnb-debian10-l>
2021-07-01 12:55:06 +0200

    Testpartie2

---
+++ fichier14.py
@@ -0,0 +1,8 @@
+URL = "http://localhost:8585/api/review-requests/3/diffs/1/files/3/patched-file/"
+
+headers = {'Authorization': 'token 8b395c24de9363275d04697e73dc41dd5b6ad9a2'}  
+# sending get request and saving the response as response object
+r = requests.get(url = URL,headers=headers)
+  
+# extracting data in json format
+data = r.content
